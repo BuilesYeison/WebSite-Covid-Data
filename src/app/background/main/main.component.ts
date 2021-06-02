1fc0d2c717e65e6ee0d806b4490faf9c4f4f7256
Yeison Stiven Builes Uribe <54378260+BuilesYeison@users.noreply.github.com>
2021-06-02 14:49:50 -0500

    Data binded to html components

--- src/app/background/main/main.component.ts
+++ src/app/background/main/main.component.ts
@@ -10,14 +10,47 @@ export class MainComponent implements OnInit {
   countries:any;
   countrySelected:string="";
   globalCovidData:any;
+  covidData:any = {
+    country:"",
+    newConfirmed:"",
+    totalConfirmed:"",
+    newDeaths:"",
+    totalDeaths:"",
+    newRecovered:"",
+    totalRecovered:""
+  };
   constructor(private getCovidData:GetApiDataService) { }
 
-  getCountrySelected(data:string){this.countrySelected = data}
+  getCountrySelected(data:string){
+    this.countrySelected = data;
+    for(let i = 0;i<this.countries.length;i++){ //get the index of the country selected and collect all covid data into a new object to bind into html elements
+      if((this.countries[i].Country).toLowerCase() === (this.countrySelected).toLowerCase()){
+        this.covidData = {
+          country: this.countries[i].Country,
+          newConfirmed: this.countries[i].NewConfirmed,
+          totalConfirmed: this.countries[i].TotalConfirmed,
+          newDeaths: this.countries[i].NewDeaths,
+          totalDeaths: this.countries[i].TotalDeaths,
+          newRecovered: this.countries[i].NewRecovered,
+          totalRecovered: this.countries[i].TotalRecovered
+        }
+      }
+    }
+  }
   ngOnInit(): void {
-    /*this.getCovidData.getData().then(data=>{
+    this.getCovidData.getData().then(data=>{
       this.countries = data.Countries;
       this.globalCovidData = data.Global;
-    });//get data when the app is initialized and send it to serach component*/
+      this.covidData = {
+        country: "Global",
+        newConfirmed:this.globalCovidData.NewConfirmed,
+        totalConfirmed:this.globalCovidData.TotalConfirmed,
+        newDeaths:this.globalCovidData.NewDeaths,
+        totalDeaths:this.globalCovidData.TotalDeaths,
+        newRecovered:this.globalCovidData.NewRecovered,
+        totalRecovered:this.globalCovidData.TotalRecovered
+      };
+    });//get data when the app is initialized and send it to search component
   }
 
 }
